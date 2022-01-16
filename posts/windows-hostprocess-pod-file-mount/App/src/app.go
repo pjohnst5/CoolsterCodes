c40ad7fcff8c05fe0e21c6ed938c29dba4073d51
Paul Johnston <johnstonpaul801@gmail.com>
2022-01-16 21:13:10 +0000

    Reading config map as file using hostprocess pod

--- posts/windows-hostprocess-pod-file-mount/App/src/app.go
+++ posts/windows-hostprocess-pod-file-mount/App/src/app.go
@@ -8,36 +8,18 @@ import (
 )
 
 func main() {
-	fmt.Println("hello world")
-	hostFile := os.Args[1]
+	configMapFileLocation := os.Args[1]
 
-	fmt.Printf("hostFile: %s\n", hostFile)
+	fmt.Printf("configMapFileLocation: %s\n", configMapFileLocation)
 
-	// Read host file contents
-	content, err := ioutil.ReadFile(hostFile)
+	// Read mounted config map now as a file!
+	fmt.Println("Reading the config map as a file!")
+	content, err := ioutil.ReadFile(configMapFileLocation)
 	if err != nil {
 		panic(err)
 	}
 	text := string(content)
 	fmt.Println(text)
-
-	// Append to host file contents
-	f, err := os.OpenFile(hostFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		panic(err)
-	}
-	if _, err = f.WriteString("\nHello from the pod"); err != nil {
-		panic(err)
-	}
-	f.Close()
-
-	// Read host file contents again
-	content2, err := ioutil.ReadFile(hostFile)
-	if err != nil {
-		panic(err)
-	}
-	text2 := string(content2)
-	fmt.Println(text2)
 
 	// Sleep
 	time.Sleep(1 * time.Hour)
