12f427bd596526854ba84a4983de60a3f2d4a0d8
Paul Johnston <johnstonpaul801@gmail.com>
2022-01-23 06:15:20 +0000

    Almost done

--- posts/windows-pod-file-mount/App/src/app.go
+++ posts/windows-pod-file-mount/App/src/app.go
@@ -17,16 +17,18 @@ func main() {
 	// Read host file from inside the container!
 	content, err := ioutil.ReadFile(hostFileToReadLocation)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error from reading: %v\n", err)
 	}
 	text := string(content)
 	fmt.Println(text)
 
 	// Write to host file from inside the container!
-	message := []byte("Hello from the container!")
-	err = ioutil.WriteFile(hostFileToWriteLocation, message, 755)
+	f, err := os.OpenFile(hostFileToWriteLocation, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		panic(err)
+		fmt.Printf("Error opening file to write: %v\n", err)
+	}
+	if _, err = f.WriteString("Hello from the container!\n"); err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
 	}
 
 	// Sleep
