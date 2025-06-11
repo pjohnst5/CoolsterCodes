164c8ffaa2fe11bd295b1214ca3e45436820de19
Paul Johnston <johnstonpaul801@gmail.com>
2025-06-11 13:57:07 -0600

    making some progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                proseBody: '#eeeeee',       // --tw-prose-body
-                proseLinks: '#5da7d8',      // --tw-prose-links
-                proseInvertBody: '#d1d5db', // --tw-prose-invert-body
-                proseInvertLinks: '#fff',   // --tw-prose-invert-links
+                myblue: '#5da7d8',
             },
             fontFamily: {
             },
