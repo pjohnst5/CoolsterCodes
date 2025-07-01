0497ac418041ef674917ae56e315a3f9ae7efeab
Paul Johnston <johnstonpaul801@gmail.com>
2025-07-01 16:44:14 -0600

    no lint ireturn

--- modules/modulir/log.go
+++ modules/modulir/log.go
@@ -130,7 +130,7 @@ type colorizer struct {
 	LogColor bool
 }
 
-func (c *colorizer) Blue(arg interface{}) aurora.Value { //nolint:all
+func (c *colorizer) Blue(arg interface{}) aurora.Value { //nolint:ireturn
 	if !c.LogColor {
 		return aurora.Reset(arg)
 	}
